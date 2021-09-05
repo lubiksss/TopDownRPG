133cae5ff3107b57592c7db2095ecd1e0793e9cf
JAEDEOKJO <lubiksss@gmail.com>
2021-09-05 16:31:40 +0900

    first_commit

---
+++ Assets/Script/PlayerAction.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAction : MonoBehaviour
+{
+
+    float h; float v;
+    public float moveSpeed;
+    Rigidbody2D rigid;
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    void FixedUpdate()
+    {
+        // plyaer move
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+        rigid.velocity = new Vector2(h * moveSpeed, v * moveSpeed);
+    }
+}
---
+++ readme.md
@@ -0,0 +1,8 @@
+## *참고
+<a href='https://www.youtube.com/watch?v=JY-KFx3OsJo&list=PLO-mt5Iu5TeYfyXsi6kzHK8kfjPvadC5u'>탑다운 2D RPG - 도트 타일맵으로 쉽게 준비하기 [유니티 기초 강좌 B20 + 에셋 다운로드]</a>
+
+Unity로 2D TopDown RPG 게임을 만들었습니다. C# Scripting으로만 구현했습니다.  
+
+## Preview
+<!-- ![normal](./images/test.JPG) -->
+<!-- ![collide](./images/collide.JPG) -->
