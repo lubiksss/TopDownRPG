faca726d017f098a74dd3b9d66ce7c0094e1a098
JAEDEOKJO <lubiksss@gmail.com>
2021-09-09 01:49:35 +0900

    update_playerAction

--- Assets/Script/PlayerAction.cs
+++ Assets/Script/PlayerAction.cs
@@ -6,11 +6,16 @@ public class PlayerAction : MonoBehaviour
 {
 
     float h; float v;
+    bool isHorizonMove;
     float moveSpeed = 5;
+    Vector3 dirVec;
+    GameObject scanObject;
     Rigidbody2D rigid;
+    Animator anim;
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
     // Start is called before the first frame update
     void Start()
@@ -21,12 +26,59 @@ public class PlayerAction : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // move value
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+
+        // Check Button Down & Up
+        bool hDown = Input.GetButtonDown("Horizontal");
+        bool vDown = Input.GetButtonDown("Vertical");
+        bool hUp = Input.GetButtonUp("Horizontal");
+        bool vUp = Input.GetButtonUp("Vertical");
+
+        // Check Horizontal Move
+        if (hDown) { isHorizonMove = true; }
+        else if (vDown) { isHorizonMove = false; }
+        else if (hUp || vUp) { isHorizonMove = h != 0; }
+
+        // Animation
+        if (anim.GetInteger("hAxisRaw") != h)
+        {
+            anim.SetBool("isChange", true);
+            anim.SetInteger("hAxisRaw", (int)h);
+        }
+        else if (anim.GetInteger("vAxisRaw") != v)
+        {
+            anim.SetBool("isChange", true);
+            anim.SetInteger("vAxisRaw", (int)v);
+        }
+        else { anim.SetBool("isChange", false); }
+
+        //Direction
+        if (vDown && v == 1) { dirVec = Vector3.up; }
+        if (vDown && v == -1) { dirVec = Vector3.down; }
+        if (hDown && h == -1) { dirVec = Vector3.left; }
+        if (hDown && h == 1) { dirVec = Vector3.right; }
+
+        //Scan Object
+        if (Input.GetButtonDown("Jump") && scanObject != null)
+        {
+            Debug.Log("this is: " + scanObject.name);
+        }
     }
     void FixedUpdate()
     {
         // plyaer move
-        h = Input.GetAxisRaw("Horizontal");
-        v = Input.GetAxisRaw("Vertical");
-        rigid.velocity = new Vector2(h * moveSpeed, v * moveSpeed);
+        Vector2 moveVec = isHorizonMove ? new Vector2(h, 0) : new Vector2(0, v);
+        rigid.velocity = moveVec * moveSpeed;
+
+        // Ray
+        Debug.DrawRay(rigid.position, dirVec * 0.7f, new Color(0, 1, 0));
+        RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, dirVec, 0.7f, LayerMask.GetMask("Object"));
+        if (rayHit.collider != null)
+        {
+            scanObject = rayHit.collider.gameObject;
+        }
+        else { scanObject = null; }
     }
 }
